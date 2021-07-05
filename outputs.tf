70bda507d8e869437f2434163f713eeca4d5f96b
Sean Sundberg <seansund@us.ibm.com>
2021-07-05 01:22:17 -0500

    Outputs service account name and namespace (#6)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "service_account_name" {
+  value = var.service_account_name
+  depends_on = [null_resource.setup_argocd, null_resource.setup_rbac]
+}
+
+output "service_account_namespace" {
+  value = var.service_account_namespace
+  depends_on = [null_resource.setup_argocd, null_resource.setup_rbac]
+}
