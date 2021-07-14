8abdf889e71e96e87046d184c36bb9b25e6f7dfa
Sean Sundberg <seansund@us.ibm.com>
2021-07-13 23:00:17 -0500

    Simplifies the inputs and updates setup logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 locals {
   layer = "infrastructure"
-  config_project = var.config_projects[local.layer]
+  layer_config = var.gitops_config[local.layer]
   application_branch = "main"
   label = var.label != null && var.label != "" ? var.label : "${var.namespace}-rbac"
-  application_repo_path = "${var.application_paths[local.layer]}/namespace/${var.namespace}"
   namespace = var.cluster_scope ? "default" : var.namespace
+  yaml_dir = "${path.cwd}/.tmp/rbac-${local.label}"
 }
 
-resource null_resource setup_rbac {
+resource null_resource setup_yaml {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-rbac.sh '${var.application_repo}' '${local.application_repo_path}' '${var.namespace}' '${var.service_account_name}' '${var.service_account_namespace}' '${local.label}' '${var.cluster_scope}'"
+    command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${var.namespace}' '${var.service_account_name}' '${var.service_account_namespace}' '${local.label}' '${var.cluster_scope}'"
 
     environment = {
-      TOKEN = var.application_token
       RULES = yamlencode(var.rules)
     }
   }
 }
 
-resource null_resource setup_argocd {
-  depends_on = [null_resource.setup_rbac]
+resource null_resource setup_gitops {
+  depends_on = [null_resource.setup_yaml]
+
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-argocd.sh '${var.config_repo}' '${var.config_paths[local.layer]}' '${local.config_project}' '${var.application_repo}' '${local.application_repo_path}' '${var.namespace}' '${local.application_branch}'"
+    command = "${path.module}/scripts/setup-gitops.sh 'namespace-${var.namespace}' '${local.yaml_dir}' 'namespace/${var.namespace}' '${local.application_branch}' '${var.namespace}'"
 
     environment = {
-      TOKEN = var.config_token
+      GIT_CREDENTIALS = jsonencode(var.git_credentials)
+      GITOPS_CONFIG = jsonencode(local.layer_config)
     }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "service_account_name" {
   value = var.service_account_name
-  depends_on = [null_resource.setup_argocd, null_resource.setup_rbac]
+  depends_on = [null_resource.setup_gitops]
 }
 
 output "service_account_namespace" {
   value = var.service_account_namespace
-  depends_on = [null_resource.setup_argocd, null_resource.setup_rbac]
+  depends_on = [null_resource.setup_gitops]
 }
