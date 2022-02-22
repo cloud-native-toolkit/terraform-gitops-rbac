60d32cc18eb83f31c490f7963cfdb2968b1eee3a
Sean Sundberg <seansund@us.ibm.com>
2022-02-22 07:12:01 -0600

    Backs out gitops provider, for now (#18)
    This reverts commit 9743c4a9f56a8819b7b2493ce2ff29afc4ec424e.

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ locals {
   namespace = var.cluster_scope ? "default" : var.namespace
   name = "${local.label}-rbac"
   yaml_dir = "${path.cwd}/.tmp/rbac-${local.label}"
-  type = "base"
   provision = length(var.rules) > 0
 }
 
@@ -31,16 +30,37 @@ resource null_resource create_yaml {
   }
 }
 
-resource gitops_module module {
+resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml]
   count = local.provision ? 1 : 0
 
-  name = local.name
-  namespace = var.namespace
-  content_dir = local.yaml_dir
-  server_name = var.server_name
-  layer = local.layer
-  type = local.type
-  config = yamlencode(var.gitops_config)
-  credentials = yamlencode(var.git_credentials)
+  triggers = {
+    name = local.name
+    namespace = var.namespace
+    yaml_dir = local.yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --debug"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
 }
